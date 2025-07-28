df0459c1064a9a44dd319c391ce2e6f822957ec2
The-Truest-Repairman <brandonpekarek91@gmail.com>
2025-07-28 16:57:30 -0700

    Final Integration script built with some output images

--- v1.r
+++ v1.r
@@ -9,6 +9,8 @@ library(EnsDb.Hsapiens.v86)
 library(ggplot2)
 library(cowplot)
 
+#SET WORKING DIRECTORY
+
 # load both modalities
 pbmc.rna <- LoadData("pbmcMultiome", "pbmc.rna")
 pbmc.atac <- LoadData("pbmcMultiome", "pbmc.atac")
@@ -40,3 +42,32 @@ pbmc.atac <- RunUMAP(pbmc.atac, reduction = "lsi", dims = 2:30, reduction.name =
 p1 <- DimPlot(pbmc.rna, group.by = "seurat_annotations", label = TRUE) + NoLegend() + ggtitle("RNA")
 p2 <- DimPlot(pbmc.atac, group.by = "orig.ident", label = FALSE) + NoLegend() + ggtitle("ATAC")
 p1 + p2
+
+plot <- (p1 + p2) & xlab("UMAP 1") & ylab("UMAP 2") & theme(axis.title = element_text(size = 18))
+# can save plot here if necessary
+ggsave(filename = "./output/images/atacseq_integration_vignette.jpg", height = 7, width = 12, plot = plot,
+    quality = 50)
+
+# quantify gene activity
+gene.activities <- GeneActivity(pbmc.atac, features = VariableFeatures(pbmc.rna))
+# add gene activities as a new assay
+pbmc.atac[["ACTIVITY"]] <- CreateAssayObject(counts = gene.activities)
+
+# normalize gene activities
+DefaultAssay(pbmc.atac) <- "ACTIVITY"
+pbmc.atac <- NormalizeData(pbmc.atac)
+pbmc.atac <- ScaleData(pbmc.atac, features = rownames(pbmc.atac))
+# Identify anchors
+transfer.anchors <- FindTransferAnchors(reference = pbmc.rna, query = pbmc.atac, features = VariableFeatures(object = pbmc.rna),
+    reference.assay = "RNA", query.assay = "ACTIVITY", reduction = "cca")
+
+#Annotate scATAC-seq cells via label transfer
+celltype.predictions <- TransferData(anchorset = transfer.anchors, refdata = pbmc.rna$seurat_annotations,
+    weight.reduction = pbmc.atac[["lsi"]], dims = 2:30)
+
+pbmc.atac <- AddMetaData(pbmc.atac, metadata = celltype.predictions)
+
+pbmc.atac$annotation_correct <- pbmc.atac$predicted.id == pbmc.atac$seurat_annotations
+p1 <- DimPlot(pbmc.atac, group.by = "predicted.id", label = TRUE) + NoLegend() + ggtitle("Predicted annotation")
+p2 <- DimPlot(pbmc.atac, group.by = "seurat_annotations", label = TRUE) + NoLegend() + ggtitle("Ground-truth annotation")
+p1 | p2
